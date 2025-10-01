39c4e5bb82c44e111345fef819e432be3a77b788
kudaT <timilehinkudaisi01@gmail.com>
2025-10-01 11:45:05 -0700

    update header

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,33 +5,39 @@ const Footer = () => {
     <footer className="w-full p-3">
       <section>
         <div className="flex flex-col space-y-8">
-          <div className="flex justify-between items-start pr-12">
-            <p>logo</p>
-            <div>
-              <h6>Menu</h6>
-              <ul>
-                <li>Studio</li>
-                <li>Contact</li>
-                <li>Work</li>
-              </ul>
+          <div className="flex justify-between items-start pr-12 md:pr-36 w-full">
+            <div className="basis-[40%] lg:basis-[100%]">
+              <p>logo</p>
             </div>
-            <div>
-              <h6>Social</h6>
-              <ul>
-                <li>Instagram</li>
-                <li>Linkedin</li>
-                <li>Behance</li>
-              </ul>
-            </div>
-          </div>
-          <div className="flex flex-col items-center space-y-8">
-            <div>
-              <h6>Business enquires</h6>
-              <p>timmy7@gmail.com</p>
-            </div>
-            <div>
-              <h6>Join our team</h6>
-              <p>apply@forman.co</p>
+            <div className="flex-1 lg:flex lg:justify-between gap-24">
+              <div className="flex justify-between w-full pb-8 lg:gap-14">
+                <div>
+                  <h6>Menu</h6>
+                  <ul>
+                    <li className="text-xs">Studio</li>
+                    <li className="text-xs">Contact</li>
+                    <li className="text-xs">Work</li>
+                  </ul>
+                </div>
+                <div>
+                  <h6>Social</h6>
+                  <ul>
+                    <li className="text-xs">Instagram</li>
+                    <li className="text-xs">Linkedin</li>
+                    <li className="text-xs">Behance</li>
+                  </ul>
+                </div>
+              </div>
+              <div className="flex flex-col items-center space-y-8 md:flex-row md:justif-center md:items-center md:space-y-0 md:justify-between lg:flex-col lg:gap-5">
+                <div>
+                  <h6>Business enquires</h6>
+                  <p>timmy7@gmail.com</p>
+                </div>
+                <div>
+                  <h6>Join our team</h6>
+                  <p>apply@forman.co</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="flex justify-between items-center p-3">
+    <header className="flex justify-between items-center p-4 fixed z-50 w-full bg-white-100">
       <p className="text-[12px]">logo</p>
       <nav>
         <ul className="flex items-center gap-3 text-mute">
